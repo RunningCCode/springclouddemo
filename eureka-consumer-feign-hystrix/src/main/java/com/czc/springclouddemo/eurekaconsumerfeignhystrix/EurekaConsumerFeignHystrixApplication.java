4a63ecb123b8dc6911450c48b6ac71d233ac9e0f
chenzhicong <383059038@qq.com>
2019-07-22 12:02:16 +0800

    学习熔断器

---
+++ eureka-consumer-feign-hystrix/src/main/java/com/czc/springclouddemo/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
@@ -0,0 +1,15 @@
+package com.czc.springclouddemo.eurekaconsumerfeignhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class EurekaConsumerFeignHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignHystrixApplication.class, args);
+    }
+
+}
--- eureka-consumer-feign-hystrix/src/main/java/com/czc/springclouddemo/eurekaconsumerfeignhystrix/controller/HelloController.java
+++ eureka-consumer-feign-hystrix/src/main/java/com/czc/springclouddemo/eurekaconsumerfeignhystrix/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.czc.springclouddemo.eurekaconsumerfeignhystrix.controller;
 
 import com.czc.springclouddemo.eurekaconsumerfeignhystrix.service.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
