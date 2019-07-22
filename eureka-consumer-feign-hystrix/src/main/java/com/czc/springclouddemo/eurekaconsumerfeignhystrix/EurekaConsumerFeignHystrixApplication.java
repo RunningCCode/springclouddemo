10fb7e62d7dbf054b11e5dafd3896c45f082dbd4
chenzhicong <383059038@qq.com>
2019-07-22 22:40:37 +0800

    学习config-server 和 hystrix仪表盘

--- eureka-consumer-feign-hystrix/src/main/java/com/czc/springclouddemo/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
+++ eureka-consumer-feign-hystrix/src/main/java/com/czc/springclouddemo/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
@@ -2,10 +2,12 @@ package com.czc.springclouddemo.eurekaconsumerfeignhystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix //启用监控
 public class EurekaConsumerFeignHystrixApplication {
 
     public static void main(String[] args) {
